84846986401c0df871b84161aa3646dacc3a29a7
Clifford Silla <clifford.silla@gmail.com>
2018-11-04 20:48:18 +0300

    Updates

---
+++ precache-manifest.4b572cfdb449a1299936eabe3538bcb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31a96741d04966067aea",
+    "url": "/react-project-planner/static/js/runtime~main.31a96741.js"
+  },
+  {
+    "revision": "f579c8ecf5f45e95dafb",
+    "url": "/react-project-planner/static/js/main.f579c8ec.chunk.js"
+  },
+  {
+    "revision": "bbbc4a07c2cd906f2c76",
+    "url": "/react-project-planner/static/js/1.bbbc4a07.chunk.js"
+  },
+  {
+    "revision": "f579c8ecf5f45e95dafb",
+    "url": "/react-project-planner/static/css/main.083609e2.chunk.css"
+  },
+  {
+    "revision": "6e2d9be7c3bb06107f080b405cef1bc0",
+    "url": "/react-project-planner/index.html"
+  }
+];
